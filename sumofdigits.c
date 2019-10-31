80aa78d85e9fdf0b6dd544a5ddabb261b55df64f
manojmadhav19 <manojmadhav19@gmail.com>
2019-10-31 19:44:04 +0530

    Optimized the code by reducing one unecessary for loop.

--- sumofdigits.c
+++ sumofdigits.c
@@ -1,28 +1,20 @@
 #include<stdio.h>
 /* This program computes the sum of the digits of the number entered */
 
-int main ()
-{
-    int result,sum=0,n,m;
-    int div = 10,counter = 0,number;
+ int main(){
+    /* This program computes the sum of the digits of the number entered */
 
-    printf("Enter the integer:");
-    scanf("%i",&number);                      /* Store the sum of the digits into the variable 'number' */
+    int a,b,sum=0;
     
-    while(result >0){                         /* WHILE LOOP */
-        result = number/div;
-        div *= 10;          
-        counter++;                            /* Count the number of digits in the number */
+    printf("Enter the number:");
+    scanf("%d",&a);                              /* Store the sum of the digits into the variable 'a' */
+
+    while (a>0){                                 /* WHILE LOOP */
+    b = a%10;                                               
+    sum += b;
+    a = a/10;  
     }
-  
-    for(int i=0;i<counter;i++){               /* FOR LOOP */
-        m = number%10;
-        sum += m;                             /* Calculate the sum of digits in the number */
-        number = number/10;
-    }
-    printf("The sum of the digits:%d \n",sum); /* Print the sum of the digits */
-    
+
+    printf("The sum of the digits:%d \n",sum);
     return 0;
-
-
-}
+} 
