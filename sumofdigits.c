3d1f1bf1dbe04ee2ba09b7b36b373b7417490987
manojmadhav19 <manojmadhav19@gmail.com>
2019-10-31 19:49:03 +0530

    Adding few final comments to the optimized code

--- sumofdigits.c
+++ sumofdigits.c
@@ -2,19 +2,17 @@
 /* This program computes the sum of the digits of the number entered */
 
  int main(){
-    /* This program computes the sum of the digits of the number entered */
-
     int a,b,sum=0;
     
     printf("Enter the number:");
-    scanf("%d",&a);                              /* Store the sum of the digits into the variable 'a' */
+    scanf("%d",&a);                  /* Store the sum of the digits into the variable 'a' */
 
-    while (a>0){                                 /* WHILE LOOP */
-    b = a%10;                                               
-    sum += b;
+    while (a>0){                     /* WHILE LOOP */
+    b = a%10;      
+    sum += b;                        /* This while loop runs until 'a' is computed down to zero & all the digits are added up*/
     a = a/10;  
     }
 
-    printf("The sum of the digits:%d \n",sum);
+    printf("The sum of the digits:%d \n",sum);   /* Printing the sum of the digits entered */
     return 0;
 } 
