d26ed4f65aaa77c378d2a5ca0f9db9aac1a011b9
manojmadhav19 <manojmadhav19@gmail.com>
2019-10-31 19:35:24 +0530

    Adding few comments to the code

--- sumofdigits.c
+++ sumofdigits.c
@@ -7,21 +7,21 @@ int main ()
     int div = 10,counter = 0,number;
 
     printf("Enter the integer:");
-    scanf("%i",&number);
+    scanf("%i",&number);                      /* Store the sum of the digits into the variable 'number' */
     
-    while(result >0){
+    while(result >0){                         /* WHILE LOOP */
         result = number/div;
         div *= 10;          
         counter++;                            /* Count the number of digits in the number */
     }
   
-    for(int i=0;i<counter;i++){
+    for(int i=0;i<counter;i++){               /* FOR LOOP */
         m = number%10;
         sum += m;                             /* Calculate the sum of digits in the number */
         number = number/10;
     }
-    printf("The sum of the digits:%d \n",sum);
-
+    printf("The sum of the digits:%d \n",sum); /* Print the sum of the digits */
+    
     return 0;
 
 
